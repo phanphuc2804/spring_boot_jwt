9c56052f558628206f3a477ddf9c702b94ddf02e
phanphuc2804 <phanphuc28042000@gmail.com>
2021-09-26 11:19:03 +0700

    add login

--- src/main/java/com/example/spring_boot_jwt_phanhoangphuc/controller/AuthController.java
+++ src/main/java/com/example/spring_boot_jwt_phanhoangphuc/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.example.spring_boot_jwt_phanhoangphuc.controller;
 
+import com.example.spring_boot_jwt_phanhoangphuc.authen.UserPrincipal;
+import com.example.spring_boot_jwt_phanhoangphuc.entity.Token;
 import com.example.spring_boot_jwt_phanhoangphuc.entity.User;
 import com.example.spring_boot_jwt_phanhoangphuc.service.TokenService;
 import com.example.spring_boot_jwt_phanhoangphuc.service.UserService;
 import com.example.spring_boot_jwt_phanhoangphuc.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,5 +29,21 @@ public class AuthController {
     public User register(@RequestBody User user){
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         return userService.createUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        UserPrincipal userPrincipal= userService.findbyUsername(user.getUsername());
+
+        if (user == null || !new BCryptPasswordEncoder().matches(user.getPassword(),
+                                                        userPrincipal.getPassword())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Account or password is not valid!");
+        }
+        Token token= new Token();
+        token.setToken(jwtUtil.generateToken(userPrincipal));
+        token.setTokenExpDate(jwtUtil.generateExpirationDate());
+        token.setCreateBy(userPrincipal.getUserId());
+        tokenService.createToken(token);
+        return ResponseEntity.ok(token.getToken());
     }
 }
